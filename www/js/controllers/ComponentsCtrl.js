a07db9fc21dac17c1f78921f571f2f7c6f655d50
riyas <riyasksalim@hotmail.com>
2017-06-01 13:18:23 -0700

    dgr

--- www/js/controllers/ComponentsCtrl.js
+++ www/js/controllers/ComponentsCtrl.js
@@ -1,4 +1,4 @@
-﻿app.controller('ComponentsCtrl', function ($scope, $stateParams, ionicMaterialInk) {
+﻿app.controller('MainCtrl', function ($scope, ionicMaterialInk, ImageSearch, $ionicSlideBoxDelegate) {
     //ionic.material.ink.displayEffect();
     ionicMaterialInk.displayEffect();
 
@@ -9,4 +9,33 @@
             this.classList.toggle('active');
         });
     }
+    debugger;
+    $scope.images = ["https://www.w3schools.com/css/img_fjords.jpg", "https://www.w3schools.com/css/img_fjords.jpg","https://www.w3schools.com/css/img_fjords.jpg"];
+
+
+
+    $scope.doSearch = function () {
+        if (!$scope.search) return;
+        console.log("search for ", $scope.search);
+        ImageSearch.getImages($scope.search).then(function (results) {
+            $scope.images = results.data.d.results;
+            setTimeout(function () {
+                $ionicSlideBoxDelegate.slide(0);
+                $ionicSlideBoxDelegate.update();
+                $scope.$apply();
+            });
+        });
+    };
 });
+
+app.service("ImageSearch", function ($http) {
+
+    return {
+        getImages: function (term) {
+            var appid = "fgQ7ve/sV/eB3NN/+fDK9ohhRWj1z1us4eIbidcsTBM";
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + btoa(appid + ':' + appid);
+            return $http.get("https://api.datamarket.azure.com/Bing/Search/v1/Image?$format=json&Query='" + escape(term) + "'&$top=10");
+        }
+    };
+
+})
